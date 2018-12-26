76c99f45dbc3c733b8ce3950f8b77019d54ef749
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2018-12-26 15:04:09 +0530

    Setup Dagger2
    Set it up in the MessApp class and make AppModule(which provides
    Application) and AppComponent.

--- app/src/main/java/org/bitspilani/ssms/messapp/MessApp.kt
+++ app/src/main/java/org/bitspilani/ssms/messapp/MessApp.kt
@@ -1,5 +1,22 @@
 package org.bitspilani.ssms.messapp
 
 import android.app.Application
+import org.bitspilani.ssms.messapp.di.app.AppComponent
+import org.bitspilani.ssms.messapp.di.app.AppModule
+import org.bitspilani.ssms.messapp.di.app.DaggerAppComponent
 
-class MessApp : Application()
+class MessApp : Application() {
+
+    companion object {
+
+        lateinit var appComponent: AppComponent
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        appComponent = DaggerAppComponent.builder()
+            .appModule(AppModule(this))
+            .build()
+    }
+}
---
+++ app/src/main/java/org/bitspilani/ssms/messapp/di/app/AppComponent.kt
@@ -0,0 +1,7 @@
+package org.bitspilani.ssms.messapp.di.app
+
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton @Component(modules = [AppModule::class])
+interface AppComponent
