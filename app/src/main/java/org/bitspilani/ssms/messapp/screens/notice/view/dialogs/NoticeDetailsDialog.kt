ba88e1d5748f78375d147931055f7b4cb8a678ab
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2018-12-27 21:18:56 +0530

    Update NoticeDetails dialog to allow notice deletion

--- app/src/main/java/org/bitspilani/ssms/messapp/screens/notice/view/dialogs/NoticeDetailsDialog.kt
+++ app/src/main/java/org/bitspilani/ssms/messapp/screens/notice/view/dialogs/NoticeDetailsDialog.kt
@@ -8,13 +8,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import kotlinx.android.synthetic.main.dia_notice_details.view.*
 import org.bitspilani.ssms.messapp.R
+import org.bitspilani.ssms.messapp.screens.notice.core.model.Id
 
 class NoticeDetailsDialog : DialogFragment() {
 
+    interface ClickListener {
+
+        fun onDeleteBTNPressed(id: Id)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        if(arguments?.getLong("ID") == null) {
+            Toast.makeText(context, "An error occurred :(", Toast.LENGTH_SHORT).show()
+            dismiss()
+        }
+        val id = arguments!!.getLong("ID")
         val heading = arguments?.getString("HEADING")?: "Error"
         val content = arguments?.getString("CONTENT")?: "Something has gone wrong"
 
@@ -25,6 +37,11 @@ class NoticeDetailsDialog : DialogFragment() {
 
         rootPOV.closeBTN.setOnClickListener {
             this.dismiss()
+        }
+
+        rootPOV.deleteBTN.setOnClickListener {
+            (parentFragment as ClickListener).onDeleteBTNPressed(id)
+            dismiss()
         }
 
         return rootPOV
