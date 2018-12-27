d276cede677d156e068c8ca1c74bbae16878b99f
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2018-12-27 20:36:05 +0530

    Update NoticeRepository to allow notice deletion

--- app/src/main/java/org/bitspilani/ssms/messapp/screens/notice/data/repo/NoticeRepository.kt
+++ app/src/main/java/org/bitspilani/ssms/messapp/screens/notice/data/repo/NoticeRepository.kt
@@ -1,9 +1,15 @@
 package org.bitspilani.ssms.messapp.screens.notice.data.repo
 
+import io.reactivex.Completable
 import io.reactivex.Observable
+import org.bitspilani.ssms.messapp.screens.notice.core.model.Id
 import org.bitspilani.ssms.messapp.screens.notice.core.model.Notice
 
 interface NoticeRepository {
 
     fun getAllNotices(): Observable<List<Notice>>
+
+    fun deleteAllNotices(): Completable
+
+    fun deleteNoticeById(id: Id): Completable
 }
