f763e1c437390c1160b980262ef9a0fe41d5ab24
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2018-12-29 18:55:22 +0530

    Make AppComponent provide UserRepository

--- app/src/main/java/org/bitspilani/ssms/messapp/di/app/AppModule.kt
+++ app/src/main/java/org/bitspilani/ssms/messapp/di/app/AppModule.kt
@@ -1,14 +1,28 @@
 package org.bitspilani.ssms.messapp.di.app
 
 import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.room.Room
 import dagger.Module
 import dagger.Provides
+import org.bitspilani.ssms.messapp.screens.shared.data.repo.UserRepository
+import org.bitspilani.ssms.messapp.screens.shared.data.repo.UserRepositoryImpl
 import org.bitspilani.ssms.messapp.screens.shared.data.room.setup.AppDatabase
 import javax.inject.Singleton
 
 @Module
 class AppModule(private val application: Application) {
+
+    @Provides @Singleton
+    fun providesUserRepository(sharedPreferences: SharedPreferences): UserRepository {
+        return UserRepositoryImpl(sharedPreferences)
+    }
+
+    @Provides @Singleton
+    fun providesSharedPreferences(application: Application): SharedPreferences {
+        return application.getSharedPreferences("mess.sp", Context.MODE_PRIVATE)
+    }
 
     @Provides @Singleton
     fun providesAppDatabase(application: Application): AppDatabase {
