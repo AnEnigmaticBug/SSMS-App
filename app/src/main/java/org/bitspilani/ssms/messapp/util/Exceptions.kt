242e805af570e5f5c6d42a583b911693e8293ba7
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-01-08 16:42:22 +0530

    Add a few execeptions(general + login-related)

--- app/src/main/java/org/bitspilani/ssms/messapp/util/Exceptions.kt
+++ app/src/main/java/org/bitspilani/ssms/messapp/util/Exceptions.kt
@@ -9,3 +9,13 @@ class NoDataSourceException(message: String) : Exception(message)
  * Thrown when the user's details can't be retrieved from the cache.
  * */
 class NoLoggedUserException(message: String) : Exception(message)
+
+/**
+ * Thrown when the device doesn't have a connection to the internet.
+ * */
+class NoConnectionException(message: String) : Exception(message)
+
+/**
+ * Thrown when the user can't be logged in with his/her credentials.
+ * */
+class InvalidCredentialsException(message: String) : Exception(message)
