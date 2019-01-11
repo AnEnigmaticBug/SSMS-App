d81ca87bb10e78193f87d120dfc4d12945c3c5d1
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-01-11 22:27:05 +0530

    Make core model objects for the feedback screen

---
+++ app/src/main/java/org/bitspilani/ssms/messapp/screens/feedback/core/model/Feedback.kt
@@ -0,0 +1,3 @@
+package org.bitspilani.ssms.messapp.screens.feedback.core.model
+
+data class Feedback(val content: String, val tags: Set<Tag>)
---
+++ app/src/main/java/org/bitspilani/ssms/messapp/screens/feedback/core/model/Tag.kt
@@ -0,0 +1,6 @@
+package org.bitspilani.ssms.messapp.screens.feedback.core.model
+
+enum class Tag {
+
+    App, Food, Hygiene, Service
+}
