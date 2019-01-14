20f90c8f0dcfc5c85f2bb6fc08b5953d7262b42c
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-01-14 08:37:11 +0530

    Make NoticeRepositoryImpl use the NoticeService
    This is the second endpoint to be integrated into the app :)

--- app/src/main/java/org/bitspilani/ssms/messapp/di/screens/notice/NoticeScreenModule.kt
+++ app/src/main/java/org/bitspilani/ssms/messapp/di/screens/notice/NoticeScreenModule.kt
@@ -1,17 +1,24 @@
 package org.bitspilani.ssms.messapp.di.screens.notice
 
+import android.content.SharedPreferences
 import dagger.Module
 import dagger.Provides
 import org.bitspilani.ssms.messapp.screens.notice.data.repo.NoticeRepository
 import org.bitspilani.ssms.messapp.screens.notice.data.repo.NoticeRepositoryImpl
+import org.bitspilani.ssms.messapp.screens.notice.data.retrofit.NoticeService
 import org.bitspilani.ssms.messapp.screens.notice.data.room.NoticesDao
 import org.bitspilani.ssms.messapp.screens.shared.data.room.setup.AppDatabase
+import org.bitspilani.ssms.messapp.util.NetworkWatcher
+import retrofit2.Retrofit
 
 @Module
 class NoticeScreenModule {
 
     @Provides
-    fun providesNoticeRepository(noticesDao: NoticesDao): NoticeRepository = NoticeRepositoryImpl(noticesDao)
+    fun providesNoticeRepository(sharedPreferences: SharedPreferences, noticesDao: NoticesDao, networkWatcher: NetworkWatcher, noticeService: NoticeService): NoticeRepository = NoticeRepositoryImpl(sharedPreferences, noticesDao, networkWatcher, noticeService)
+
+    @Provides
+    fun noticeService(retrofit: Retrofit): NoticeService = retrofit.create(NoticeService::class.java)
 
     @Provides
     fun providesNoticesDao(appDatabase: AppDatabase): NoticesDao = appDatabase.noticesDao()
--- app/src/main/java/org/bitspilani/ssms/messapp/screens/notice/data/repo/NoticeRepositoryImpl.kt
+++ app/src/main/java/org/bitspilani/ssms/messapp/screens/notice/data/repo/NoticeRepositoryImpl.kt
@@ -1,49 +1,112 @@
 package org.bitspilani.ssms.messapp.screens.notice.data.repo
 
+import android.annotation.SuppressLint
+import android.content.SharedPreferences
+import androidx.core.content.edit
 import io.reactivex.Completable
+import io.reactivex.Flowable
 import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import org.bitspilani.ssms.messapp.screens.notice.core.model.Id
 import org.bitspilani.ssms.messapp.screens.notice.core.model.Notice
 import org.bitspilani.ssms.messapp.screens.notice.core.model.Priority
+import org.bitspilani.ssms.messapp.screens.notice.data.retrofit.NoticeService
 import org.bitspilani.ssms.messapp.screens.notice.data.room.NoticesDao
 import org.bitspilani.ssms.messapp.screens.notice.data.room.model.DataLayerNotice
+import org.bitspilani.ssms.messapp.util.NetworkWatcher
+import org.bitspilani.ssms.messapp.util.NoDataSourceException
+import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalDateTime
+import org.threeten.bp.LocalTime
 
-class NoticeRepositoryImpl(private val noticesDao: NoticesDao) : NoticeRepository {
+class NoticeRepositoryImpl(
+    private val prefs: SharedPreferences,
+    private val noticesDao: NoticesDao,
+    private val networkWatcher: NetworkWatcher,
+    private val noticeService: NoticeService
+) : NoticeRepository {
 
-    init {
-        Completable.fromAction {
-            noticesDao.deleteAllNotices()
-            noticesDao.insertNotices(
-                listOf(
-                    DataLayerNotice(1, "Change in dinner timings", "Yeah. Sad but true", Priority.Important, LocalDateTime.now()),
-                    DataLayerNotice(2, "Release of SSMS App", "Now, that's awesome", Priority.Critical, LocalDateTime.now().minusDays(3)),
-                    DataLayerNotice(3, "Lorem Ipsum", "Dolor sit amet", Priority.Normal, LocalDateTime.now().minusDays(1)),
-                    DataLayerNotice(4, "Mess Food Wastage", "12kg of food was wasted", Priority.Important, LocalDateTime.now().minusDays(1)),
-                    DataLayerNotice(5, "Bolo tara rara", "Perty ytreP", Priority.Normal, LocalDateTime.now().minusWeeks(1)),
-                    DataLayerNotice(6, "Generic Heading", "Generic Content", Priority.Normal, LocalDateTime.now().minusMonths(1))
-                )
-            )
-        }.subscribeOn(Schedulers.io()).subscribe()
-    }
-
+    @SuppressLint("CheckResult")
     override fun getAllNotices(): Observable<List<Notice>> {
-        return noticesDao.getAllNotices()
+        return noticesDao.getNoticeCount()
+            .take(1)
+            .flatMap { _count ->
+                when {
+                    _count <= 0 -> {
+                        when(networkWatcher.isConnectedToInternet()) {
+                            false -> throw NoDataSourceException("Not connected to the internet")
+                            true  -> Flowable.concat(fetchAndInsertNotices().toFlowable(), noticesDao.getAllNotices())
+                        }
+                    }
+                    else        -> Flowable.merge(fetchAndInsertNotices().toFlowable(), noticesDao.getAllNotices())
+                }
+            }
+            .onErrorResumeNext(noticesDao.getAllNotices())
+            .map { _notices ->
+                _notices.map { Notice(it.id, it.heading, it.content, it.priority, it.datetime) }
+            }
             .subscribeOn(Schedulers.io())
-            .map { it.map { Notice(it.id, it.heading, it.content, it.priority, it.datetime) } }
             .toObservable()
     }
 
     override fun deleteAllNotices(): Completable {
         return Completable.fromAction {
+            val newlyDeletedIds = noticesDao.getAllIds().toSet().map { it.toString() }
+            val deletedIds = HashSet(prefs.getStringSet("DELETED_IDS", mutableSetOf())!!)
+            deletedIds.addAll(newlyDeletedIds)
+
+            prefs.edit(commit = true) {
+                putStringSet("DELETED_IDS", deletedIds)
+            }
+
             noticesDao.deleteAllNotices()
         }.subscribeOn(Schedulers.io())
     }
 
     override fun deleteNoticeById(id: Id): Completable {
         return Completable.fromAction {
+            val deletedIds = HashSet(prefs.getStringSet("DELETED_IDS", mutableSetOf())!!)
+            deletedIds.add(id.toString())
+
+            prefs.edit(commit = true) {
+                putStringSet("DELETED_IDS", deletedIds)
+            }
+
             noticesDao.deleteNoticeById(id)
         }.subscribeOn(Schedulers.io())
+    }
+
+    private fun fetchAndInsertNotices(): Single<List<DataLayerNotice>> {
+        return noticeService.getNotices()
+            .map { _response ->
+                when(_response.code()) {
+                    200  -> _response.body()!!
+                    else -> throw Exception("${_response.code()}: ${_response.errorBody()?.string()}")
+                }
+            }
+            .map { _notices ->
+                val deletedIds = prefs.getStringSet("DELETED_IDS", setOf())!!.map { it.toLong() }
+                _notices.filter { it.id !in deletedIds }
+            }
+            .map { _notices ->
+                _notices.map {
+                    val priority = when(it.priority) {
+                        "C"  -> Priority.Critical
+                        "I"  -> Priority.Important
+                        "N"  -> Priority.Normal
+                        else -> throw IllegalArgumentException("Invalid notice priority: ${it.priority}")
+                    }
+
+                    val datetime = LocalDateTime.of(LocalDate.parse(it.date), LocalTime.NOON)
+
+                    DataLayerNotice(it.id, it.heading, it.content, priority, datetime)
+                }
+            }
+            .doOnSuccess { _notices ->
+                noticesDao.deleteAllNotices()
+                noticesDao.insertNotices(_notices)
+            }
+            .subscribeOn(Schedulers.io())
     }
 }
