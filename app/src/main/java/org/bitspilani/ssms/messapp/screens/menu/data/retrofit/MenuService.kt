60f1bf2c1c40bfe573614b969f769fc51cf6c444
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-01-16 00:13:51 +0530

    Add support for rating menu items in retrofit service

--- app/src/main/java/org/bitspilani/ssms/messapp/screens/menu/data/retrofit/MenuService.kt
+++ app/src/main/java/org/bitspilani/ssms/messapp/screens/menu/data/retrofit/MenuService.kt
@@ -1,12 +1,20 @@
 package org.bitspilani.ssms.messapp.screens.menu.data.retrofit
 
+import io.reactivex.Completable
 import io.reactivex.Single
+import okhttp3.RequestBody
 import org.bitspilani.ssms.messapp.screens.menu.data.retrofit.model.MealResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface MenuService {
 
     @GET("api/mess/menu")
     fun getMeals(): Single<Response<List<MealResponse>>>
+
+    @POST("api/mess/menu")
+    fun rateItems(@Header("JWT") jwt: String, @Body body: RequestBody): Completable
 }
