a5049e27ba978fba3a98ad2a50a18cd087dd3df5
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-01-16 00:18:10 +0530

    Setup di for SendRatingWorker and schedule it

--- app/src/main/java/org/bitspilani/ssms/messapp/MessApp.kt
+++ app/src/main/java/org/bitspilani/ssms/messapp/MessApp.kt
@@ -1,10 +1,13 @@
 package org.bitspilani.ssms.messapp
 
 import android.app.Application
+import androidx.work.*
 import com.jakewharton.threetenabp.AndroidThreeTen
 import org.bitspilani.ssms.messapp.di.app.AppComponent
 import org.bitspilani.ssms.messapp.di.app.AppModule
 import org.bitspilani.ssms.messapp.di.app.DaggerAppComponent
+import org.bitspilani.ssms.messapp.screens.menu.work.SendRatingWorker
+import java.util.concurrent.TimeUnit
 
 class MessApp : Application() {
 
@@ -21,5 +24,17 @@ class MessApp : Application() {
             .build()
 
         AndroidThreeTen.init(this)
+    }
+
+    private fun scheduleWork() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        val work = PeriodicWorkRequestBuilder<SendRatingWorker>(12, TimeUnit.HOURS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance().enqueueUniquePeriodicWork("RATE_ITEMS", ExistingPeriodicWorkPolicy.KEEP, work)
     }
 }
