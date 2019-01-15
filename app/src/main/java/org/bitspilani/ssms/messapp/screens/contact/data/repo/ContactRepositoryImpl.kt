0faf5bb93644699c569dff639611cc1ca49d847f
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-01-15 23:55:16 +0530

    Make ContactRepository's implementation using Room and Retrofit

---
+++ app/src/main/java/org/bitspilani/ssms/messapp/screens/contact/data/repo/ContactRepositoryImpl.kt
@@ -0,0 +1,58 @@
+package org.bitspilani.ssms.messapp.screens.contact.data.repo
+
+import io.reactivex.Flowable
+import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+import org.bitspilani.ssms.messapp.screens.contact.core.model.Contact
+import org.bitspilani.ssms.messapp.screens.contact.data.retrofit.ContactService
+import org.bitspilani.ssms.messapp.screens.contact.data.room.ContactsDao
+import org.bitspilani.ssms.messapp.screens.contact.data.room.model.DataLayerContact
+import org.bitspilani.ssms.messapp.util.NetworkWatcher
+import org.bitspilani.ssms.messapp.util.NoDataSourceException
+
+class ContactRepositoryImpl(
+    private val contactsDao: ContactsDao,
+    private val networkWatcher: NetworkWatcher,
+    private val contactService: ContactService
+) : ContactRepository {
+
+    override fun getAllContacts(): Observable<List<Contact>> {
+        return contactsDao.getContactCount()
+            .take(1)
+            .flatMap { _count ->
+                when {
+                    _count <= 0 -> {
+                        when(networkWatcher.isConnectedToInternet()) {
+                            true  -> Flowable.concat(fetchAndUpdateContacts().toFlowable(), contactsDao.getAllContacts())
+                            false -> throw NoDataSourceException("Not connected to the internet")
+                        }
+                    }
+                    else        -> Flowable.merge(fetchAndUpdateContacts().toFlowable(), contactsDao.getAllContacts())
+                }
+            }
+            .onErrorResumeNext(contactsDao.getAllContacts())
+            .map { _contacts -> _contacts.map { Contact(it.name, it.post, it.picUrl, it.phone) } }
+            .filter { it.isNotEmpty() }
+            .subscribeOn(Schedulers.io())
+            .toObservable()
+    }
+
+    private fun fetchAndUpdateContacts(): Single<List<DataLayerContact>> {
+        return contactService.getAllContacts()
+            .map { _response ->
+                when(_response.code()) {
+                    200  -> _response.body()!!
+                    else -> throw Exception("${_response.code()}: ${_response.errorBody()?.string()}")
+                }
+            }
+            .map { _contacts ->
+                _contacts.map { DataLayerContact(it.name, it.post, it.picUrl, it.phone) }
+            }
+            .doOnSuccess { _contacts ->
+                contactsDao.deleteAllContacts()
+                contactsDao.insertContacts(_contacts)
+            }
+            .subscribeOn(Schedulers.io())
+    }
+}
