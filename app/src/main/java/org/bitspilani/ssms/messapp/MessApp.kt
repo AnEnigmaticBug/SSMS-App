3ae3bcb92116bcd9327ac5f8948eeea8124ef438
Nishant Mahajan <nishant.mahajanXVII@gmail.com>
2019-01-29 10:23:17 +0530

    Add the RxJavaPlugins error handler
    This change will prevent UndeliverableExceptions from taking down the
    app.

--- app/src/main/java/org/bitspilani/ssms/messapp/MessApp.kt
+++ app/src/main/java/org/bitspilani/ssms/messapp/MessApp.kt
@@ -1,7 +1,9 @@
 package org.bitspilani.ssms.messapp
 
 import android.app.Application
+import android.util.Log
 import com.jakewharton.threetenabp.AndroidThreeTen
+import io.reactivex.plugins.RxJavaPlugins
 import org.bitspilani.ssms.messapp.di.app.AppComponent
 import org.bitspilani.ssms.messapp.di.app.AppModule
 import org.bitspilani.ssms.messapp.di.app.DaggerAppComponent
@@ -19,6 +21,10 @@ class MessApp : Application() {
         appComponent = DaggerAppComponent.builder()
             .appModule(AppModule(this))
             .build()
+
+        RxJavaPlugins.setErrorHandler {
+            Log.e("MessApp", "MessApp: Undeliverable exception of type $it: ${it.message?: "No message"}")
+        }
 
         AndroidThreeTen.init(this)
     }
